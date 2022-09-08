8cf8469be5ce0c289cfaa16d4d99fe6219b25121
Philipp Theis <jpth@lambda9.de>
2022-09-08 09:46:46 +0200

    Update text on home

--- src/Page.elm
+++ src/Page.elm
@@ -26,4 +26,4 @@ pageContentView page =
 
 pageInitView : Html msg
 pageInitView =
-    div [] [ text "Hi, I am Philipp. I am a Software Developer from Flensburg Germany. On this Page you will find something about me and my projects. Have fun while exploring it" ]
+    div [] [ text "Hi, on this page you will find something about me and my projects. Have fun while exploring it" ]
